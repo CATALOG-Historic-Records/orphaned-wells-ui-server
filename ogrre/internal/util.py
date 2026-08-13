beb5790995b7e0686815ca7e99fd2905b71b6acc
rtl9148 <robert.t.loza@gmail.com>
2026-08-13 12:24:09 -0500

    black reformat fix

--- ogrre/internal/util.py
+++ ogrre/internal/util.py
@@ -638,7 +638,9 @@ def cleanRecordAttribute(processor_attributes, attribute, subattributeKey=None):
         else:
             cleaning_function = CLEANING_FUNCTIONS.get(cleaning_function_name)
             if cleaning_function:
-                options = attribute_schema.get("options") or {} if attribute_schema else {}
+                options = (
+                    attribute_schema.get("options") or {} if attribute_schema else {}
+                )
                 try:
                     cleaned_val = cleaning_function(unclean_val, options=options)
                     _log.debug(f"CLEANED: {unclean_val} : {cleaned_val}")
